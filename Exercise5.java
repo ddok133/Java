d6ab556389c85d85835cfee1f9d42d67ce0563e2
ddok133 <ddok133@gmail.com>
2020-06-19 16:57:28 +0800

    20200619 Add Erercise5

---
+++ Exercise5.java
@@ -0,0 +1,35 @@
+class A {
+	public String Name = "A大樓設計圖";
+	public int Size = 30;
+	public String Price = "300萬";
+	public int Quantity = 20;
+}
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		A A1;
+		A1 = new A();
+		A1.Name = "A1大樓";
+		A1.Price = "350萬";
+
+		System.out.println("Name : " + A1.Name);
+		System.out.println("Size :" + A1.Size);
+		System.out.println("Price :" + A1.Price);
+		System.out.println("Quantity :" + A1.Quantity);
+		System.out.println();
+
+		A A2 = new A(); // 另一個寫法
+		A2.Name = "A2大樓";
+		A2.Quantity = 30;
+
+		System.out.println("Name : " + A2.Name);
+		System.out.println("Size : " + A2.Size);
+		System.out.println("Price : " + A2.Price);
+		System.out.println("Quantity : " + A2.Quantity);
+
+	}
+
+}
