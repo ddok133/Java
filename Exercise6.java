487ecd7f9635075beec694bc259cbede21b82961
ddok133 <ddok133@gmail.com>
2020-06-22 10:49:03 +0800

    20200622 Add Exercise6

---
+++ Exercise6.java
@@ -0,0 +1,28 @@
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "Hello";
+		String s2 = s1;
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+		
+		
+		s1 = s1.concat(" ya");
+		s2 = s2.concat(" ya");
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+		
+		s1 = s1.concat(" you");
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+		
+		
+		
+		
+				
+	}
+
+}
