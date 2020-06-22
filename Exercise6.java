3014177b4c57d49ccf7b33db0915ba15c2781822
ddok133 <ddok133@gmail.com>
2020-06-22 10:59:42 +0800

    20200622 modify Exercise6

--- Exercise6.java
+++ Exercise6.java
@@ -6,23 +6,18 @@ public class Exercise6 {
 
 		String s1 = "Hello";
 		String s2 = s1;
-		System.out.println(s1 == s2);
-		System.out.println(s1.equals(s2));
-		
-		
+		System.out.println("s1 == s2 : " + (s1 == s2));
+		System.out.println("s1 equals : " + s1.equals(s2));			
+
 		s1 = s1.concat(" ya");
 		s2 = s2.concat(" ya");
-		System.out.println(s1 == s2);
-		System.out.println(s1.equals(s2));
-		
+		System.out.println("s1 == s2 : " + (s1 == s2));
+		System.out.println("s1 equals : " + s1.equals(s2));
+
 		s1 = s1.concat(" you");
-		System.out.println(s1 == s2);
-		System.out.println(s1.equals(s2));
-		
-		
-		
-		
-				
+		System.out.println("s1 == s2 : " + (s1 == s2));
+		System.out.println("s1 equals : " + s1.equals(s2));
+
 	}
 
 }
